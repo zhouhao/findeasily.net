10d946e4e26096dcb29ce59300f78869400294e8
Hao Zhou <hzhou.me@gmail.com>
2018-05-22 21:20:09 -0400

    set mapper dir in resource folder

--- src/main/java/net/findeasily/website/generator/MyBatisGenerator.java
+++ src/main/java/net/findeasily/website/generator/MyBatisGenerator.java
@@ -1,15 +1,23 @@
 package net.findeasily.website.generator;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.aeonbits.owner.ConfigFactory;
 
 import com.baomidou.mybatisplus.enums.IdType;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.FileOutConfig;
 import com.baomidou.mybatisplus.generator.config.GlobalConfig;
 import com.baomidou.mybatisplus.generator.config.PackageConfig;
 import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.TemplateConfig;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
 import com.baomidou.mybatisplus.generator.config.rules.DbType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.google.common.collect.Lists;
 
 
 public class MyBatisGenerator {
@@ -46,11 +54,37 @@ public class MyBatisGenerator {
                 .setFileOverride(true)
                 .setServiceName("%sService");
 
+        TemplateConfig tc = new TemplateConfig()
+                .setXml(null);
+
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                Map<String, Object> map = new HashMap<>();
+                map.put("abc", this.getConfig().getGlobalConfig().getAuthor() + "-mp");
+                this.setMap(map);
+            }
+        };
+
+        cfg.setFileOutConfigList(Lists.newArrayList(new FileOutConfig("/templates/mapper.xml.vm") {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                return "src/main/resources/mybatis/mapper/" + tableInfo.getEntityName() + "Mapper.xml";
+            }
+        }));
+
+        PackageConfig pc = new PackageConfig()
+                .setParent(packageName)
+                .setEntity("domain")
+                .setController("controller");
+
         new AutoGenerator()
+                .setCfg(cfg)
+                .setTemplate(tc)
                 .setGlobalConfig(config)
                 .setDataSource(dataSourceConfig)
                 .setStrategy(strategyConfig)
-                .setPackageInfo(new PackageConfig().setParent(packageName).setEntity("domain"))
+                .setPackageInfo(pc)
                 .execute();
 
     }
