595e4de3b85be3a5de97fc39c1f5804aa3f6aa30
Hao Zhou <hzhou.me@gmail.com>
2018-05-23 23:28:40 -0400

    configure smtp service

---
+++ src/main/java/net/findeasily/website/service/EmailService.java
@@ -0,0 +1,25 @@
+package net.findeasily.website.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    private JavaMailSender javaMailSender;
+
+    @Autowired
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public void sendMail(String toEmail, String subject, String message) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(toEmail);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(message);
+        mailMessage.setFrom("contact@mail.findeasily.net");
+        javaMailSender.send(mailMessage);
+    }
+}
