63d6447f18ff001f1f0134f5e2c3871679c190a3
Hao Zhou <hzhou.me@gmail.com>
2018-06-23 20:23:12 -0400

    update config and use hzhou as test profile

--- src/test/java/net/findeasily/website/ApplicationTests.java
+++ src/test/java/net/findeasily/website/ApplicationTests.java
@@ -3,10 +3,12 @@ package net.findeasily.website;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("hzhou")
 public class ApplicationTests {
 
     @Test
