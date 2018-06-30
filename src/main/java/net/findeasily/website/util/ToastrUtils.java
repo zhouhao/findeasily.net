237472113243c27b814e9222a600c0c0b7c238f1
Hao Zhou <hzhou.me@gmail.com>
2018-06-30 16:41:37 -0400

    improve toastr support to display info/error message

---
+++ src/main/java/net/findeasily/website/util/ToastrUtils.java
@@ -0,0 +1,26 @@
+package net.findeasily.website.util;
+
+import javax.validation.constraints.NotNull;
+
+public final class ToastrUtils {
+
+    public static String success(@NotNull String msg) {
+        return build("success", msg);
+    }
+
+    public static String info(@NotNull String msg) {
+        return build("info", msg);
+    }
+
+    public static String warn(@NotNull String msg) {
+        return build("warning", msg);
+    }
+
+    public static String error(@NotNull String msg) {
+        return build("error", msg);
+    }
+
+    private static String build(String type, String msg) {
+        return String.format("toastr.%s('%s');", type, msg.replace("'", "\'"));
+    }
+}
