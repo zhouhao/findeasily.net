9e17f82a68e41b1ff75e2bd292e77851a3fa2ec4
Hao Zhou <hzhou.me@gmail.com>
2018-06-29 21:26:28 -0400

    add configuration for dev and test env

--- src/test/java/net/findeasily/website/ApplicationTests.java
+++ src/test/java/net/findeasily/website/ApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("hzhou")
+@ActiveProfiles("test")
 public class ApplicationTests {
 
     @Test
