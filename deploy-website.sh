4767fc46af6932b781b83f90504666f15ae1f667
Hao Zhou <hzhou@cargurus.com>
2018-11-07 16:51:11 -0500

    fix deploy script

--- deploy-website.sh
+++ deploy-website.sh
@@ -6,7 +6,7 @@ SSH_USER=root
 SSH_HOST=findeasily.net
 mvn clean package
 
-scp -i script/findeasily_rsa target/website.jar $SSH_USER@$SSH_HOST:/root/
+scp -i script/findeasily_rsa target/website.jar $SSH_USER@$SSH_HOST:/var/findeasily/
 ssh -i script/findeasily_rsa $SSH_USER@$SSH_HOST "systemctl restart website.service"
 
 echo "The website will be ready in about 1 minute!"
