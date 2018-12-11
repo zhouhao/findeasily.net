d52834cb3eb55a2c4c1b72a537333ea4c773362c
Hao Zhou <i@hzhou.me>
2018-12-10 23:58:22 -0500

    #14 add some userService unit tests

--- src/test/java/net/findeasily/website/ApplicationTests.java
+++ src/test/java/net/findeasily/website/ApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("dev")
+@ActiveProfiles("test")
 public class ApplicationTests {
 
     @Test
