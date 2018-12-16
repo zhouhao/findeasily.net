0d1bfae6d4cf272e739ec15d0050f5f28a0ab473
Hao Zhou <i@hzhou.me>
2018-12-16 15:03:32 -0500

    auto create a dev user for dev and test profiles for fast development

---
+++ src/main/java/net/findeasily/website/schedule/start/DevProfileStartup.java
@@ -0,0 +1,53 @@
+package net.findeasily.website.schedule.start;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+import net.findeasily.website.domain.Role;
+import net.findeasily.website.domain.User;
+import net.findeasily.website.domain.form.UserCreateForm;
+import net.findeasily.website.service.UserService;
+
+@Component
+@Profile({"dev", "test"})
+@Slf4j
+public class DevProfileStartup implements CommandLineRunner {
+    private final UserService userService;
+
+    @Autowired
+    public DevProfileStartup(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        createDevUser();
+    }
+
+    private void createDevUser() {
+        User user = userService.getUserByEmail("test@test.com");
+        if (user != null) {
+            return;
+        }
+
+        String testPwd = "test123!";
+        String testEmail = "test@test.com";
+        UserCreateForm ucf = new UserCreateForm();
+        ucf.setEmail(testEmail);
+        ucf.setPassword(testPwd);
+        ucf.setPasswordRepeated(testPwd);
+        ucf.setRole(Role.USER);
+        ucf.setUsername(RandomStringUtils.randomAlphabetic(6));
+        user = userService.create(ucf);
+        if (user == null) {
+            log.error("failed to create test user: {}", ucf);
+            return;
+        }
+        userService.activate(user.getId());
+        log.info("Dev user is created now, you can login with `{}` with password `{}`", testEmail, testPwd);
+    }
+}
